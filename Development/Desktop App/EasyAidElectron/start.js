6428fbc0b8e25561ed9c9aef5d26c824a692f403
Manuel Murtas <manuel.murtas3@gmail.com>
2019-03-19 08:42:16 +0100

    Reformatting

--- Development/Desktop App/EasyAidElectron/start.js
+++ Development/Desktop App/EasyAidElectron/start.js
@@ -4,23 +4,23 @@ const { app, BrowserWindow } = require('electron')
 // chiusa automaticamente quando l'oggetto JavaScript sarà garbage collected.
 let win
 
-function createWindow () {
-  // Creazione della finestra del browser.
-  win = new BrowserWindow({ width: 800, height: 600, frame: false})
+function createWindow() {
+	// Creazione della finestra del browser.
+	win = new BrowserWindow({ width: 800, height: 600, frame: false })
 
-  // e viene caricato il file index.html della nostra app.
-  win.loadFile('src/SplashScreen.html')
+	// e viene caricato il file index.html della nostra app.
+	win.loadFile('src/SplashScreen.html')
 
-  // Apre il Pannello degli Strumenti di Sviluppo.
-  //win.webContents.openDevTools()
+	// Apre il Pannello degli Strumenti di Sviluppo.
+	//win.webContents.openDevTools()
 
-  // Emesso quando la finestra viene chiusa.
-  win.on('closed', () => {
-    // Eliminiamo il riferimento dell'oggetto window;  solitamente si tiene traccia delle finestre
-    // in array se l'applicazione supporta più finestre, questo è il momento in cui 
-    // si dovrebbe eliminare l'elemento corrispondente.
-    win = null
-  })
+	// Emesso quando la finestra viene chiusa.
+	win.on('closed', () => {
+		// Eliminiamo il riferimento dell'oggetto window;  solitamente si tiene traccia delle finestre
+		// in array se l'applicazione supporta più finestre, questo è il momento in cui 
+		// si dovrebbe eliminare l'elemento corrispondente.
+		win = null
+	})
 }
 
 // Questo metodo viene chiamato quando Electron ha finito
@@ -30,19 +30,19 @@ app.on('ready', createWindow)
 
 // Terminiamo l'App quando tutte le finestre vengono chiuse.
 app.on('window-all-closed', () => {
-  // Su macOS è comune che l'applicazione e la barra menù 
-  // restano attive finché l'utente non esce espressamente tramite i tasti Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	// Su macOS è comune che l'applicazione e la barra menù 
+	// restano attive finché l'utente non esce espressamente tramite i tasti Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', () => {
-  // Su macOS è comune ri-creare la finestra dell'app quando
-  // viene cliccata l'icona sul dock e non ci sono altre finestre aperte.
-  if (win === null) {
-    createWindow()
-  }
+	// Su macOS è comune ri-creare la finestra dell'app quando
+	// viene cliccata l'icona sul dock e non ci sono altre finestre aperte.
+	if (win === null) {
+		createWindow()
+	}
 })
 
 // in questo file possiamo includere il codice specifico necessario 
