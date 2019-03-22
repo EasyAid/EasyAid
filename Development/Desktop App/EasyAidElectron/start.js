92ab19b34ec819643594f3e4fbb4d5aa47258d38
Manuel Murtas <manuel.murtas3@gmail.com>
2019-03-22 18:05:04 +0100

    Electron Change Window Test

--- Development/Desktop App/EasyAidElectron/start.js
+++ Development/Desktop App/EasyAidElectron/start.js
@@ -1,50 +1,56 @@
 const { app, BrowserWindow } = require('electron')
 
-// Mantiene un riferimento globale all'oggetto window, se non lo fai, la finestra sarà
-// chiusa automaticamente quando l'oggetto JavaScript sarà garbage collected.
-let win
+let SplashScreen = null
+let Empty = null
 
-function createWindow(url, w, h) {
-	// Creazione della finestra del browser.
-	win = new BrowserWindow({ width: w, height: h, frame: false, resizable: false })
-
-	// e viene caricato il file index.html della nostra app.
-	win.loadFile(url.toString())
-
-	// Apre il Pannello degli Strumenti di Sviluppo.
-	//win.webContents.openDevTools()
-
-	// Emesso quando la finestra viene chiusa.
-	win.on('closed', () => {
-		// Eliminiamo il riferimento dell'oggetto window;  solitamente si tiene traccia delle finestre
-		// in array se l'applicazione supporta più finestre, questo è il momento in cui 
-		// si dovrebbe eliminare l'elemento corrispondente.
-		win = null
+function CreateSplashScreen() {
+	SplashScreen = new BrowserWindow({
+		width: 800,
+		height: 400,
+		frame: false,
+		resizable: false,
+		show: false,
+		backgroundColor: '#00796A'
 	})
+	
+	SplashScreen.loadFile('src/SplashScreen.html')
+
+	SplashScreen.once('ready-to-show', () => {
+        SplashScreen.show()
+        SplashScreen.on('closed', () => {
+            SplashScreen = null
+        })
+    })
 }
 
-// Questo metodo viene chiamato quando Electron ha finito
-// l'inizializzazione ed è pronto a creare le finestre browser.
-// Alcune API possono essere utilizzate solo dopo che si verifica questo evento.
+function CreateEmpty() {
+	Empty = new BrowserWindow({ 
+        parent: SplashScreen, 
+        modal: true, 
+        show: false,
+        width: 1280,
+        height: 720
+	})
+	
+	Empty.loadFile('src/empty.html')
+	
+    Empty.once('ready-to-show', () => {
+        Empty.show()
+    })
+}
+
 app.on('ready', () => {
-	createWindow("src/SplashScreen.html", 800, 400)
+    CreateSplashScreen()
 })
 
-// Terminiamo l'App quando tutte le finestre vengono chiuse.
 app.on('window-all-closed', () => {
-	// Su macOS è comune che l'applicazione e la barra menù 
-	// restano attive finché l'utente non esce espressamente tramite i tasti Cmd + Q
 	if (process.platform !== 'darwin') {
 		app.quit()
 	}
 })
 
 app.on('activate', () => {
-	// Su macOS è comune ri-creare la finestra dell'app quando
-	// viene cliccata l'icona sul dock e non ci sono altre finestre aperte.
 	if (win === null) {
-		createWindow("src/SplashScreen.html", 800, 400)
+		CreateSplashScreen()
 	}
 })
-// in questo file possiamo includere il codice specifico necessario 
-// alla nostra app. Si può anche mettere il codice in file separati e richiederlo qui.
