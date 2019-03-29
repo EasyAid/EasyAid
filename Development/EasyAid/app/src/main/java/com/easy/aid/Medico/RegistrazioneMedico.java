c13fce569dc6e6e9509a11fd7d777303f5c6de62
Cristian Tironi <tironi18@gmail.com>
2019-03-29 19:15:32 +0100

    Tiru-Rago :lobster:

--- Development/EasyAid/app/src/main/java/com/easy/aid/Medico/RegistrazioneMedico.java
+++ Development/EasyAid/app/src/main/java/com/easy/aid/Medico/RegistrazioneMedico.java
@@ -2,16 +2,34 @@ package com.easy.aid.Medico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.easy.aid.R;
 
 public class RegistrazioneMedico extends AppCompatActivity {
+
+    private RelativeLayout registrazione1, registrazione2;
+    private Button continua1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.medico_registrazione);
 
+
+        registrazione1 = (RelativeLayout) findViewById(R.id.registrazione1Med);
+        registrazione2 = (RelativeLayout) findViewById(R.id.registrazione1Med);
+        continua1 = (Button) findViewById(R.id.continua1Med);
+
+        continua1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrazione1.setVisibility(View.GONE);
+                registrazione2.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 }
