fe322ac188cb25a3938f982decb71c5457e40d9c
Cristian Tironi <tironi18@gmail.com>
2019-04-08 18:10:05 +0200

    Update

--- Development/EasyAid/app/src/main/java/com/easy/aid/Paziente/ImpostazioniPaziente.java
+++ Development/EasyAid/app/src/main/java/com/easy/aid/Paziente/ImpostazioniPaziente.java
@@ -2,6 +2,7 @@ package com.easy.aid.Paziente;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.easy.aid.R;
 
@@ -11,5 +12,8 @@ public class ImpostazioniPaziente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paziente_impostazioni);
+
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
--- Development/EasyAid/app/src/main/java/com/easy/aid/Paziente/OrdinaRicettaPaziente.java
+++ Development/EasyAid/app/src/main/java/com/easy/aid/Paziente/OrdinaRicettaPaziente.java
@@ -2,6 +2,7 @@ package com.easy.aid.Paziente;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.easy.aid.R;
 
@@ -11,5 +12,7 @@ public class OrdinaRicettaPaziente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paziente_ordina_ricetta);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 }
